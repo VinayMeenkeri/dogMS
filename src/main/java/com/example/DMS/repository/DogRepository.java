4e70bcdaac01b0137d15f9e4df756519ece963e3
VinayMeenkeri <vinay.meenkeri93@gmail.com>
2023-11-25 18:28:29 -0600

    first commit

---
+++ src/main/java/com/example/DMS/repository/DogRepository.java
@@ -0,0 +1,11 @@
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Dog;
+/**
+ * @author Vinay Meenkeri S554069
+ */
+public interface DogRepository extends CrudRepository<Dog, Integer>{
+
+}
---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+
+
+/**
+ * @author Vinay Meenkeri S554069
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
